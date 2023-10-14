865353419cd549a75589d3bf41e7bc63ede861df
best <cleargoal01@gmail.com>
2023-10-14 10:06:44 +0300

    create Entity 'Shorten'

---
+++ src/Entity/Shorten.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ShortenRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ShortenRepository::class)]
+class Shorten
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $source_url = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $hashed_url = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSourceUrl(): ?string
+    {
+        return $this->source_url;
+    }
+
+    public function setSourceUrl(string $source_url): self
+    {
+        $this->source_url = $source_url;
+
+        return $this;
+    }
+
+    public function getHashedUrl(): ?string
+    {
+        return $this->hashed_url;
+    }
+
+    public function setHashedUrl(string $hashed_url): self
+    {
+        $this->hashed_url = $hashed_url;
+
+        return $this;
+    }
+}
